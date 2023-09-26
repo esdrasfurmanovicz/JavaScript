dbd2c0f07272dcdfbf5a1c79aa84fe4f8072ee30
Esdras Furmanovicz <128192976+esdrasfurmanovicz@users.noreply.github.com>
2023-09-26 14:12:33 -0300

    exercicios

---
+++ exercicios/aula021/script.js
@@ -0,0 +1,7 @@
+/*
+const tresHoras = 60 *60 * 3 * 1000
+const data = new Date(0 + tresHoras) */
+//Date(0) = 01/01/1970 Timestamp unix ou epoca unix
+// const data = new Date(2023, 8, 16, 18, 8, 30)  ano, mes, dia, hr, min, seg, mils
+const data = new Date()
+console.log(data.toString())
